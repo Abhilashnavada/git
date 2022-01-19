bd5a277aca76e46776080ba7604f09b905bacdb0
Surabhi <Surabhi@DESKTOP-RIARN66>
2022-01-19 15:18:58 +0530

    First commit . . .

---
+++ src/git/TEST1.java
@@ -0,0 +1,17 @@
+package git;
+
+public class TEST1 {
+
+		public static void main(String[] args) {
+			String hungry = "yes";
+			if(hungry == "yes")
+				System.out.println("eat something");
+			else {
+				System.out.println("do work");
+			}
+		}
+
+
+		}
+
+
